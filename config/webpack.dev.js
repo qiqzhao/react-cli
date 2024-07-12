cdad8f738c36a9e101c20a4e9221b1f1815acc6c
qiqzhao <bme_ritter@foxmail.com>
2024-07-12 10:23:35 +0800

    chrome: init react app

---
+++ config/webpack.dev.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const EslintPlugin = require("eslint-webpack-plugin");
+const HTMLPlugin = require("html-webpack-plugin");
+const { extension } = require("mime");
+
+const getStyleLoader = (pre) => {
+  return [
+    "style-loader",
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: ["postcss-preset-env"],
+        },
+      },
+    },
+    pre,
+  ].filter(Boolean);
+};
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: undefined,
+    filename: "static/js/[name].js",
+    chunkFilename: "static/js/[name].chunk.js",
+    assetModuleFilename: "static/media/[hash:10][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: getStyleLoader(),
+      },
+      {
+        test: /\.less$/,
+        use: getStyleLoader("less-loader"),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: getStyleLoader("sass-loader"),
+      },
+      {
+        test: /\.styl$/,
+        use: getStyleLoader("stylus-loader"),
+      },
+      {
+        test: /\.(jpe?g|png|gif|webp|svg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(woff2?|ttf)/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true,
+          cacheCompression: false,
+          plugins: ["@babel/plugin-transform-runtime"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new EslintPlugin({
+      context: path.resolve(__dirname, "../src"),
+      exclude: "node_modules",
+      cacheLocation: path.resolve(
+        __dirname,
+        "../node_modules/.cache/.eslintcache"
+      ),
+    }),
+    new HTMLPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`,
+    },
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+  },
+  devServer: {
+    host: "localhost",
+    port: "3000",
+    open: true,
+    hot: true,
+  },
+  mode: "development",
+  devtool: "cheap-module-source-map",
+};
